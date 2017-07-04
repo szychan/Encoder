a0f21534c1dd83aad8ec4478bfa6d31040409016
szychan <szychan@gmail.com>
2017-07-04 18:21:53 +0200

    added RSA Key Generator layout

--- src/application/controls/RSAKeyGeneratorTabController.java
+++ src/application/controls/RSAKeyGeneratorTabController.java
@@ -1,5 +1,17 @@
 package application.controls;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class RSAKeyGeneratorTabController {
+
+    @FXML
+    private Button button_Create_RSA_Key;
+
+    @FXML
+    void button_Create_RSA_Key_Action(ActionEvent event) {
+
+    }
 
 }
